a09e466bb511a4a321861e6a5d41b851cf9590de
Júlia Penz <juliapenz_@hotmail.com>
2023-09-26 16:09:53 +0100

    changing layout

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
 const form = document.querySelector("form");
+const Iemail = document.querySelector(".email");
+const Ipassword = document.querySelector(".password");
 
-console.log(form);
+function register() {
+
+    fetch('http://localhost:8080/register',
+        {
+            headers: {
+                'Accept': 'application/json',
+                'Content-type': 'application/json'
+            },
+            method: "POST",
+            body: JSON.stringify({
+                email: Iemail.value,
+                password: Ipassword.value
+            })
+        })
+
+        .then(function (res) { console.log(res) })
+        .catch(function (res) { console.log(res) })
+};
+
+function cleaning() {
+    Iemail.value = "",
+    Ipassword.value = ""
+}
+
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    register();
+    cleaning();
+});
